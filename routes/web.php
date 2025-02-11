e372056cfaa11ae66334903b40fd05dec1b854d6
Leonardo <133729985+Th3Lover@users.noreply.github.com>
2025-02-10 21:03:38 -0300

    005

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World";
-});
-
-route::get('/about', function () {
-    echo "About Us";
-});
-
-route::get('/main/{value}', [MainController::class, 'index']); 
-route::get('/page2/{value}', [MainController::class, 'page2']); 
-route::get('/page3/{value}', [MainController::class, 'page3']); 
