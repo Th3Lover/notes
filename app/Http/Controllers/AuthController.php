2b6855f9a31bab445a9169b05b272f52b5b9602f
Leonardo <133729985+Th3Lover@users.noreply.github.com>
2025-02-12 09:37:43 -0300

    010

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,9 +15,19 @@ class AuthController extends Controller
     {
         // form validation
         $request->validate([
-            'text_username' => 'required',
-            'text_password' => 'required'
-        ]);
+            //rules
+            'text_username' => 'required|email',
+            'text_password' => 'required|min:6|max:16'
+        ],
+        // error messages
+        [
+            'text_username.required' => 'O username é obrigatório',
+            'text_username.email' => 'Username deve ser um email válido',
+            'text_password.required' => 'A password é obrigatória',
+            'text_password.min' => 'A password deve ter no mínimo :min caracteres',
+            'text_password.max' => 'A password deve ter no máximo :max caracteres'
+        ]
+        );
 
         //get yser input
         $username = $request->input('text_username');
