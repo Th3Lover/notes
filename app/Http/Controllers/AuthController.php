9d77f39a46395f2673e9c7be6420c2ca5e0b7475
Leonardo <133729985+Th3Lover@users.noreply.github.com>
2025-02-12 12:01:20 -0300

    014

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -34,15 +35,26 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // test database connection
-        try {
-            DB::connection()->getPdo();
-            echo 'Connected successfully';
-        } catch (\PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
+        //check if user exists
+        $user = User::where('username', $username)->where('deleted_at', NULL)->first();
+
+        if(!$user){
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos');
         }
 
-        echo "fim";
+        //check if password is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos');
+        }
+
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save(); //update
+
+        //login user
+        session(['user' => ['id' => $user->id, 'username' => $user->username]]);
+
+        echo 'login com sucesso';
     }
 
     public function logout()
