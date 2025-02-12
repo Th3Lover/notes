336ffd9c2c99dae83a07148a382a10d0d933b72b
Leonardo <133729985+Th3Lover@users.noreply.github.com>
2025-02-12 09:06:21 -0300

    009

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,17 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo 'Login Submit';
+        // form validation
+        $request->validate([
+            'text_username' => 'required',
+            'text_password' => 'required'
+        ]);
+
+        //get yser input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK';
     }
 
     public function logout()
